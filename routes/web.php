d60f59822b367778e8a2dda42e8e7f9d7879003a
khunnat <khunnat1223@gmail.com>
2024-03-17 17:39:29 +0700

    mimit

---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use Inertia\Inertia;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/test', function () {
+
+    return Inertia::render('Test');
+})->name('test');
+
+Route::get('/test', function () {
+    return Inertia::render('Test');
+})->name('test');
+
+Route::get('/test/{locale}', function (string $locale) {
+    if (! in_array($locale, ['en', 'kh',])) {
+        abort(400);
+    }
+
+    App::setLocale($locale);
+
+    return Inertia::render('Test');
+})->name('Tests');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
